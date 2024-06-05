03aab87e8b9a007c67ad37aa306eb16efeb50b9c
Andrei Harenkov <pevec1@yandex.ru>
2024-06-05 07:40:30 +0300

    update 2024-06-05 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react'
 import './App.css'
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function Listing({list, onClick}) {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box sx={{ border: 1, width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+      <List>
+      {[...list].map(item => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton onClick={() => onClick(item)}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+     ))}
+       </List>
+</nav>
+     </Box>
   )
+}
+
+function Details({state}) {
+  console.log(state)
+  return (
+    <Box sx={{ border: 1, width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+      <List>
+      <img src={state.avatar+"?" + Math.random()} />
+      </List>
+      <List><h1>{state.name}</h1></List>
+      <List><p>City: {state?.details?.city}</p></List>
+      <List><p>Company: {state?.details?.company}</p></List>
+      <List><p>Position: {state?.details?.position}</p></List>
+      </nav>
+     </Box>
+
+  )
+}
+function App() {
+  const [state, setState] = useState(0);
+  const [list, setList] = useState([]);
+
+  function loadData(item) {
+    if (item===undefined) {
+    setTimeout(() => {
+      fetch(
+        "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setList(data);
+        });
+    }, 100);
+  } else {
+        fetch(
+      "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/" +
+        String(item.id) +
+        ".json"
+    )
+      .then((response) => response.json())
+      .then((data) => setState(data));
+  }
+  }
+
+  useEffect(loadData, []); // componentDidMount
+
+   return (
+    <div className="wrapper">
+      <div className="list">
+        <Listing onClick={(item) => loadData(item)} list={list} />
+      </div>
+      <div className="description">
+        {(state!==0)? (<Details state={state} />) : (<p>Loading...</p>) }
+      </div>
+    </div>
+  );
 }
 
 export default App
